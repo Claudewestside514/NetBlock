573407322482260b0566044e38d2cea7baef02dc
MONA <121730102+MeowDump@users.noreply.github.com>
2026-03-11 14:05:28 +0530

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,1 +1,14 @@
 #!/system/bin/sh
+IPTABLES="/system/bin/iptables"
+IP6TABLES="/system/bin/ip6tables"
+
+# Remove all NetBlock rules
+for CMD in "$IPTABLES" "$IP6TABLES"; do
+    $CMD -F NETBLOCK 2>/dev/null
+    $CMD -D OUTPUT -j NETBLOCK 2>/dev/null
+    $CMD -X NETBLOCK 2>/dev/null
+done
+
+# Remove data
+rm -rf /data/adb/netblock
+rm -rf /data/adb/modules/netblock
